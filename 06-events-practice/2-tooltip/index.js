ecfbe838323c917d1977b29c0a61810ef1013540
kizilov vladimir <79923913+Gerandum@users.noreply.github.com>
2022-07-04 15:20:29 +0300

    Update index.js

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,21 +1,33 @@
 class Tooltip {
-  static onliInstanse = null
-  initialize () {
-    document.body.addEventListener('pointerover',(event)=>{
-  if(event.target.dataset.tooltip!=undefined){
-    this.render(event.target.dataset.tooltip)}})
+  static op;
+  initialize() {
+    document.body.addEventListener("pointerover", (event) => {
+      if (event.target.dataset.tooltip != undefined) {
+        if (Tooltip.op) {
+          this.remove();
+        }
+        this.renderD(event.target.dataset.tooltip);
+      }
+    });
+    document.body.addEventListener("pointerout", this.destroy());
   }
-  
-getTooltip(event){
-
-}
-
-  render(pip){
-    const op = document.createElement('div')
-    op.classList.add('tooltip')
-    op.innerText =  pip
-    document.body.append(op)
+  renderD(pip) {
+    const op = document.createElement("div");
+    op.classList.add("tooltip");
+    op.innerText = pip;
+    Tooltip.op = op;
+    document.body.append(Tooltip.op);
+  }
+  remove() {
+    
+    if (Tooltip.op) {
+      Tooltip.op.remove();
+      Tooltip.op = null;
     }
+  }
+  destroy() {
+    this.remove();
+  }
 }
 
 export default Tooltip;
